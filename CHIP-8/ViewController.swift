903d9112d73334b972103fe6974621716ac7caa3
Ryan Grey <ryan.g.grey@gmail.com>
2021-01-22 22:59:59 +0000

    Add CHIP-8 project

---
+++ CHIP-8/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CHIP-8
+//
+//  Created by Ryan Grey on 22/01/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ CHIP-8Tests/CHIP_8Tests.swift
@@ -0,0 +1,33 @@
+//
+//  CHIP_8Tests.swift
+//  CHIP-8Tests
+//
+//  Created by Ryan Grey on 22/01/2021.
+//
+
+import XCTest
+@testable import CHIP_8
+
+class CHIP_8Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
