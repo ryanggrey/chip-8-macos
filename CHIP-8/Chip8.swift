c33f3797298e86883e40d5f1e24a9bc37a1f4ae1
Ryan Grey <ryan.g.grey@gmail.com>
2021-02-04 20:23:19 +0000

    Allow Chip8 to be cycled and expose pixels for rendering

--- CHIP-8/Chip8.swift
+++ CHIP-8/Chip8.swift
@@ -10,14 +10,19 @@ import Foundation
 struct NotImplemented: Error {}
 
 public class Chip8 {
-    private var state = ChipState()
+    private var state: ChipState
     private let opExecutor: OpHandler
 
-    init() {
-        opExecutor = OpExecutor()
+    init(state: ChipState) {
+        self.state = state
+        self.opExecutor = OpExecutor()
     }
 
-    public func performCycle() {
-        // TODO:
+    public var pixels: [Byte] {
+        return state.pixels
+    }
+
+    public func cycle() {
+        self.state = try! opExecutor.handle(state: self.state, op: state.currentOp)
     }
 }
