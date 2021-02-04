78906cd3c9a452bb3c9bf65ec9da802c893b4059
Ryan Grey <ryan.g.grey@gmail.com>
2021-02-04 20:35:48 +0000

    Setup ViewController to load roms then trigger timer to fire cpu cycles

--- CHIP-8/ViewController.swift
+++ CHIP-8/ViewController.swift
@@ -9,18 +9,45 @@ import Cocoa
 
 class ViewController: NSViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var chip8View: Chip8View!
+    private var chip8: Chip8!
+    private var timer: Timer?
+    private let hz: TimeInterval = 1/600
 
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    private func runRomSelectorModal() {
+        RomSelectorModal.runModal { [weak self] loadedRom in
+            self?.runEmulator(with: loadedRom)
         }
     }
 
+    private func runEmulator(with rom: [Byte]) {
+        Disassembler.disassemble(codeBuffer: rom)
+        let chipState = ChipState(ram: rom)
+        self.chip8 = Chip8(state: chipState)
+        timer = Timer.scheduledTimer(
+            timeInterval: hz,
+            target: self,
+            selector: #selector(self.timerFired),
+            userInfo: nil,
+            repeats: true
+        )
+    }
 
+    @objc private func timerFired() {
+        chip8.cycle()
+        render(pixels: chip8.pixels)
+    }
+
+    private func render(pixels: [Byte]) {
+        chip8View.bitmap = pixels
+    }
+}
+
+// handle inputs
+extension ViewController {
+    @IBAction func loadRomPressed(_ sender: NSButton) {
+        timer?.invalidate()
+        self.runRomSelectorModal()
+    }
 }
 
